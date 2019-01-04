bbf78d3076a21ab57efeba6145327d36a652b33e
baemincheon <baemincheon@gmail.com>
2019-01-04 18:40:13 +0900

    refactoring Intro scene

--- Assets/Resources/SceneDeploy/Intro/IntroManager.cs
+++ Assets/Resources/SceneDeploy/Intro/IntroManager.cs
@@ -6,28 +6,20 @@ using UnityEngine.UI;
 
 public class IntroManager : MonoBehaviour
 {
-    private GameObject panel = null;
-    private Text notice = null;
+    private Text mNotice = null;
 
-    private Color blink_color = Color.white;
-    private float blink_count = 1.0f;
+    private Color mBlinkColor = Color.white;
+    private float mBlinkCount = 1.0f;
 
     private void Awake()
     {
-        this.panel = GameObject.Find("Canvas/PNotice");
-        var children_text = this.panel.GetComponentsInChildren<Text>();
-        foreach (var child in children_text)
-        {
-            if (child.name == "Text")
-            {
-                this.notice = child;
-            }
-        }
+        this.mNotice = GameObject.Find("Canvas/Panel/Notice").GetComponent<Text>();
     }
 
     private void Update()
     {
         this.ProcessInput();
+
         this.BlinkNotice();
     }
 
@@ -46,16 +38,16 @@ public class IntroManager : MonoBehaviour
 
     private void BlinkNotice()
     {
-        if(this.blink_count >= 1.0f)
+        if(this.mBlinkCount >= 1.0f)
         {
-            this.blink_count = 0.0f;
+            this.mBlinkCount = 0.0f;
         }
         else
         {
-            this.blink_count += 0.025f;
+            this.mBlinkCount += 0.025f;
         }
 
-        this.blink_color.r = this.blink_color.g = this.blink_color.b = this.blink_count;
-        this.notice.color = this.blink_color;
+        this.mBlinkColor.r = this.mBlinkColor.g = this.mBlinkColor.b = this.mBlinkCount;
+        this.mNotice.color = this.mBlinkColor;
     }
 }
