31db94874c8498106f833ed4a8b89eaa39b98df1
baemincheon <baemincheon@gmail.com>
2019-01-12 14:07:24 +0900

    implement hit animation

--- Assets/Resources/Scenes/Game/Zombie.cs
+++ Assets/Resources/Scenes/Game/Zombie.cs
@@ -12,6 +12,8 @@ public class Zombie : MonoBehaviour
         SPECIAL
     };
 
+    private UnityEngine.Object mAnimHit = null;
+
     // 플레이어 스크립트
     private Player mPlayer = null;
     // 피버 스크립트
@@ -31,8 +33,20 @@ public class Zombie : MonoBehaviour
         this.mType = type;
     }
 
+    public void Hit(int damage)
+    {
+        // 좀비의 생명력을 대미지만큼 감소시키고, 생명력이 0 에 도달했을 경우 죽는다
+        this.mLife = this.mLife - damage;
+        if (this.mLife <= 0.0f)
+        {
+            this.Die();
+        }
+    }
+
     internal void Awake()
     {
+        mAnimHit = Resources.Load("Prefabs/AnimHit");
+
         this.mPlayer = GameObject.Find("Player").GetComponent<Player>();
         this.mFever = GameObject.Find("Player").GetComponent<Fever>();
 
@@ -48,34 +62,37 @@ public class Zombie : MonoBehaviour
         this.mRuntime += Time.deltaTime;
         if (this.mRuntime >= this.mLifetime)
         {
-            this.mPlayer.Hit();
+            this.Attack();
+
             this.Die();
         }
     }
 
     internal void OnDestroy()
     {
-        // 이 좀비가 특수좀비일 경우 폭탄 아이템을 증가시킨다
-        if (this.mType == EType.SPECIAL)
-        {
-            this.mPlayer.GainBomb();
-        }
         // 좀비가 공격을 당해 죽었을 경우 점수를 얻는다
         if (this.mRuntime < this.mLifetime)
         {
             this.mFever.GainFeverCount();
             this.mPlayer.GainScore(10);
+
+            // 이 좀비가 특수좀비일 경우 폭탄 아이템을 증가시킨다
+            if (this.mType == EType.SPECIAL)
+            {
+                this.mPlayer.GainBomb();
+            }
         }
     }
 
-    public void Hit(int damage)
+    private void Attack()
     {
-        // 좀비의 생명력을 대미지만큼 감소시키고, 생명력이 0 에 도달했을 경우 죽는다
-        this.mLife = this.mLife - damage;
-        if(this.mLife <= 0.0f)
-        {
-            this.Die();
-        }
+        // 좀비가 공격하는 애니메이션을 생성하고, 이 애니메이션을 0.5 초 뒤에 삭제한다
+        GameObject obj = Instantiate(mAnimHit, this.transform) as GameObject;
+        obj.transform.SetParent(this.transform.parent);
+        Destroy(obj, 0.5f);
+
+        // 플레이어를 공격한다
+        mPlayer.Hit();
     }
 
     private void Die()
