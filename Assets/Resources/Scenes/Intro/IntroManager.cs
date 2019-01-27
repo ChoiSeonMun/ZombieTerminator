a9bbe59dbdf2845b0cbdd3581d30e4d74b96d4a3
SeonMun Choi <ch002717@gmail.com>
2019-01-27 10:43:13 +0900

    Revert "Merge branch 'Bugfix/authentication' into 'develop'"
    This reverts commit f3e90804a500ecbab82c276f980c8393f7aa81b1, reversing
    changes made to e9a3ad1ccd8db044dad75fcd516128952d69dc69.

--- Assets/Resources/Scenes/Intro/IntroManager.cs
+++ Assets/Resources/Scenes/Intro/IntroManager.cs
@@ -8,10 +8,11 @@ using GooglePlayGames.BasicApi;
 
 public class IntroManager : MonoBehaviour
 {
-    public Text NoticeText;
-    public float BlinkSpeed = 2.0f;
-    
+    // touch to start 문구를 출력하기 위한 Text
+    private Text mNotice = null;
+    // touch to start 문구를 깜빡이게 하기 위한 변수들
     private Color mBlinkColor = Color.white;
+    private float mBlinkCount = 1.0f;
 
     void Start()
     {
@@ -22,18 +23,52 @@ public class IntroManager : MonoBehaviour
         PlayGamesPlatform.DebugLogEnabled = true;
         PlayGamesPlatform.Activate();
 
-        NoticeText.text = "Touch to Start";
+        // Google ID로 유저 인증
+        PlayGamesPlatform.Instance.Authenticate((bool bSuccess) =>
+        {
+            Debug.Log("Authentication: " + bSuccess);
+        }, false);
+    }
+
+    void Awake()
+    {
+        mNotice = GameObject.Find("Canvas/Panel/Notice").GetComponent<Text>();
     }
 
     void Update()
     {
-        // 다음 씬으로 넘어가기
+        ProcessInput();
+
+        BlinkNotice();
+    }
+
+    private void ProcessInput()
+    {
         if (Input.GetMouseButtonUp(0))
         {
-            SceneManager.LoadScene("Main");
+            LoadMain();
         }
+    }
 
-        // 문구 깜빡이기
-        NoticeText.color = Color.Lerp(Color.white, Color.black, Mathf.PingPong(Time.time * BlinkSpeed, 1));
+    private void LoadMain()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    private void BlinkNotice()
+    {
+        // 카운터가 1.0 보다 커지면 == 문구가 흰색이 된다면
+        // 카운터를 0.0 값으로 한다 == 문구를 검은색으로 한다
+        // 그 외의 경우 카운터를 조금씩 증가시킨다 == 문구를 밝게 변화시킨다
+        if(mBlinkCount >= 1.0f)
+        {
+            mBlinkCount = 0.0f;
+        }
+        else
+        {
+            mBlinkCount += 0.025f;
+        }
+        mBlinkColor.r = mBlinkColor.g = mBlinkColor.b = mBlinkCount;
+        mNotice.color = mBlinkColor;
     }
 }
