36443eee29e203eb1186d881e5f502f31acdd1ae
CODESMITH-SIRYU <siryu2409@gmail.com>
2019-03-12 06:24:30 +0900

    add credit button and texts

---
+++ Assets/Resources/Scripts/EscapeSupport/CloseCredit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseCredit : MonoBehaviour
+{
+    public MainManager mainManager = null;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            mainManager.CloseCredit();
+        }
+    }
+}
